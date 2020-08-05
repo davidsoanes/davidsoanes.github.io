89b39e84e8abae0af359e3aa0b7442faf6746063
David Soanes <david.soanes@gmail.com>
2020-08-05 09:28:57 +0200

    fixes

--- js/javascript.js
+++ js/javascript.js
@@ -1,21 +1,26 @@
-//Get the button
-var mybutton = document.getElementById("myBtn");
+jQuery(document).ready(function () {
+  //   insert back to top button dynamicly
+  $("#backToTop").append(
+    '<a class="backToTop" href="javascript:void(null);" style="display: none;"><i class="fa fa-angle-up"></i><iframe id="tmp_downloadhelper_iframe" style="display: none;"></iframe></a>'
+  );
+  var $window = $(window);
+  var distance = 80;
+  // scroll
+  $window.scroll(function () {
+    // header
+    if ($window.scrollTop() >= distance) {
+      $(".backToTop").fadeIn();
+    } else {
+      $(".backToTop").fadeOut();
+    }
+  });
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+  $(".backToTop").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      800
+    );
+  });
+});
