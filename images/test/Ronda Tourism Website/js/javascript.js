b7018bbfac138ebaf4dfae7033faad222c0ab0fc
David Soanes <david.soanes@gmail.com>
2021-02-01 16:00:28 +0100

    menu update

--- images/test/Ronda Tourism Website/js/javascript.js
+++ images/test/Ronda Tourism Website/js/javascript.js
@@ -72,3 +72,18 @@ function form_validation()
    
     return true; 
 }
+
+
+
+
+if (screen.width <= 1024) {
+  function toggleMenu() {
+var menuBox = document.getElementById('menu-box');    
+if(menuBox.style.display == "block") { // if is menuBox displayed, hide it
+menuBox.style.display = "none";
+}
+else { // if is menuBox hidden, display it
+menuBox.style.display = "block";
+}
+}
+}
